3a435636b66c050a7a9517146a2aa471a9160a37
Brian Baker <brian.baker@markit.com>
2014-02-11 16:18:13 -0700

    Create io_github_brianbaker_helloworld.js

---
+++ js/io_github_brianbaker_helloworld.js
@@ -0,0 +1,14 @@
+define([
+  'F2/Events',
+  'F2/Constants'
+], function(Events, Constants) {
+
+  console.log(Events, Constants);
+
+  return {
+    init: function() {
+    },
+    dispose: function() {
+    }
+  };
+});
