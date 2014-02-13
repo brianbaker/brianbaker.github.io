4866a7276ebc0403fe8bbe1dfd9ac2c54075e086
Brian Baker <brian.baker@markit.com>
2014-02-13 14:38:19 -0700

    Update f2-amd.js

--- js/f2-amd.js
+++ js/f2-amd.js
@@ -22,7 +22,7 @@ define('F2', function() {
 		function formatUrl(name, id){
 			var paramRegex = /!(.+)/,
 				url = name.replace(paramRegex, ''),
-				param = (paramRegex.test(name))? name.replace(/.+!|:.+/, '') : DEFAULT_PARAM_NAME;
+				param = (paramRegex.test(name))? name.replace(/.+!|:.+/g, '') : DEFAULT_PARAM_NAME;
 			url += (url.indexOf('?') < 0)? '?' : '&';
 			return url + param +'='+ id;
 		}
