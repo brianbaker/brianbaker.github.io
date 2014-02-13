93bacbd0d9575c824744d78d7d1e35706b0a3010
Brian Baker <brian.baker@markit.com>
2014-02-13 10:37:56 -0700

    reorganizing, adding manifest

--- js/apps/helloworld/io_github_brianbaker_helloworld.js
+++ js/apps/helloworld/io_github_brianbaker_helloworld.js
@@ -1,12 +1,13 @@
 define([
   'F2/Events',
-  'F2/Constants'
-], function(Events, Constants) {
+  'F2/Constants',
+  'jquery-1.11.0'
+], function(Events, Constants, $) {
 
   return function() {
     return {
       init: function() {
-        console.log(Events, Constants);
+        console.log(Events, Constants, $);
       },
       dispose: function() {
       }
