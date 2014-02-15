006af75d0c846556b171952cbd6bac17dd8c5358
Brian Baker <brian.baker@markit.com>
2014-02-14 20:28:31 -0700

    Update io_github_brianbaker_helloworld.js

--- js/apps/helloworld/io_github_brianbaker_helloworld.js
+++ js/apps/helloworld/io_github_brianbaker_helloworld.js
@@ -1,16 +1,3 @@
-define([
-  'F2/Events',
-  'F2/Constants',
-  'jquery-1.11.0'
-], function(Events, Constants, $) {
-
-  return function() {
-    return {
-      init: function() {
-        console.log(Events, Constants, $);
-      },
-      dispose: function() {
-      }
-    };
-  };
+define([], function() {
+  return 'test';
 });
