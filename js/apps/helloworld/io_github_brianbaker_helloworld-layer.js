152bf1fb414a92083260900cd847b55219301c70
Brian Baker <brian.baker@markit.com>
2014-02-14 20:39:22 -0700

    Update io_github_brianbaker_helloworld-layer.js

--- js/apps/helloworld/io_github_brianbaker_helloworld-layer.js
+++ js/apps/helloworld/io_github_brianbaker_helloworld-layer.js
@@ -1,17 +1,20 @@
 // layer file for io_github_brianbaker_helloworld app
 define('io_github_brianbaker_helloworld-layer', function() {
 
+  console.log('a');
+
   define('jquery-1.11.0', ['//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'], function(jQuery) {
     return jQuery.noConflict(true);
   });
   
-  console.log('a');
+  console.log('b');
+  
   define('io_github_brianbaker_helloworld', [
     'F2/Events',
     'F2/Constants',
     'jquery-1.11.0'
   ], function(Events, Constants, $) {
-  console.log('b');
+  
     return function() {
       return {
         init: function() {
@@ -22,7 +25,8 @@ define('io_github_brianbaker_helloworld-layer', function() {
       };
     };
   });
+  
   console.log('c');
   
-  return 'test';
+  return 'test layer';
 });
